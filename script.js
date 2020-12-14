34667585f976342f88f001eff4a6f85b36621ee9
Carly Peyton <carlypeyton@gmail.com>
2020-12-13 22:30:59 -0500

    Added empty strings for new password character group and randomly generated password, to add new values to.

--- script.js
+++ script.js
@@ -14,27 +14,32 @@ var confirmUpperCase;
 var confirmNumber;
 var confirmSpecialCharacters;
 
+//Empty strings for the final password, called randomlyGeneratedPassword, and the password character options
+//where the random characters from within the 4 character-type strings will be added
+var randomlyGeneratedPassword = "";
+var passwordCharacters = "";
+
 // Write password to the #password input
 function writePassword() {
   length = parseInt(prompt("How many characters do you want your password to be? Password must be between 8 to 128 characters long."));
-    //If answer to length not a number answer, less than 8 or greater than 128 characters, user is prompted to enter number again
-    if (length < 8 || length > 128) {
-        alert("Password must be more than 8 characters and less than 128. Please try again.");
-        return;
-    } else if (isNaN(length) === true) {
-        alert("Invalid response. Please try again.");
-        return;
-    }
-    //User choices for characters. Confirm to include.
-    confirmLowerCase = confirm("Do you want to include lowercase characters in your password?");
-    confirmUpperCase = confirm("Do you want to include uppercase characters in your password?");
-    confirmNumber = confirm("Do you want to include numbers in your password?");
-    confirmSpecialCharacters = confirm("Do you want to include special characters in your password?");
-    //If user does not confirm any character types, they are alerted they must select at least one character type
-    if (confirmLowerCase === false && confirmUpperCase === false && confirmNumber === false && confirmSpecial === false) {
-        alert("Please select at least one character type.");
-        return;
-    } else { }
+  //If answer to length not a number answer, less than 8 or greater than 128 characters, user is prompted to enter number again
+  if (length < 8 || length > 128) {
+    alert("Password must be more than 8 characters and less than 128. Please try again.");
+    return;
+  } else if (isNaN(length) === true) {
+    alert("Invalid response. Please try again.");
+    return;
+  }
+  //User choices for characters. Confirm to include.
+  confirmLowerCase = confirm("Do you want to include lowercase characters in your password?");
+  confirmUpperCase = confirm("Do you want to include uppercase characters in your password?");
+  confirmNumber = confirm("Do you want to include numbers in your password?");
+  confirmSpecialCharacters = confirm("Do you want to include special characters in your password?");
+  //If user does not confirm any character types, they are alerted they must select at least one character type
+  if (confirmLowerCase === false && confirmUpperCase === false && confirmNumber === false && confirmSpecial === false) {
+    alert("Please select at least one character type.");
+    return;
+  } else { }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -53,24 +58,24 @@ document.getElementById("password").innerHTML = randomlyGeneratedPassword;
 //If character type selected, a random character from within that string type is added to the passwordCharacters string.
 function generatePassword() {
   randomlyGeneratedPassword = "";
-    if (confirmLowerCase) {
-        passwordCharacters += lowerCase;
-    }
-    if (confirmUpperCase) {
-        passwordCharacters += upperCase;
-    }
-    if (confirmNumber) {
-        passwordCharacters += numbers;
-    }
-    if (confirmSpecialCharacters) {
-        passwordCharacters += specialCharacters;
-    }
-     //As long as the length of the password is less than the length the user input, a random 
-    //character from within the passwordCharacters will be selected and added to randomlyGeneratedPassword
-    for (let i = 0; i < length; i++) {
-      randomlyGeneratedPassword += passwordCharacters.charAt(
-          Math.floor(Math.random() * passwordCharacters.length)
-      );
+  if (confirmLowerCase) {
+    passwordCharacters += lowerCase;
+  }
+  if (confirmUpperCase) {
+    passwordCharacters += upperCase;
+  }
+  if (confirmNumber) {
+    passwordCharacters += numbers;
+  }
+  if (confirmSpecialCharacters) {
+    passwordCharacters += specialCharacters;
+  }
+  //As long as the length of the password is less than the length the user input, a random 
+  //character from within the passwordCharacters will be selected and added to randomlyGeneratedPassword
+  for (let i = 0; i < length; i++) {
+    randomlyGeneratedPassword += passwordCharacters.charAt(
+      Math.floor(Math.random() * passwordCharacters.length)
+    );
   }
   //Return FINAL PASSWORD
   return randomlyGeneratedPassword;
