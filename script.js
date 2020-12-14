331b2f4f6f90cff393e424e802db92c92d96a59d
Carly Peyton <carlypeyton@gmail.com>
2020-12-13 22:21:37 -0500

    Created prompts/confirms for each character type. Created alerts to notify user if invalid choice is made, and user must hit button to generate password again.

--- script.js
+++ script.js
@@ -16,11 +16,28 @@ var confirmSpecialCharacters;
 
 // Write password to the #password input
 function writePassword() {
+  length = parseInt(prompt("How many characters do you want your password to be? Password must be between 8 to 128 characters long."));
+    //If answer to length not a number answer, less than 8 or greater than 128 characters, user is prompted to enter number again
+    if (length < 8 || length > 128) {
+        alert("Password must be more than 8 characters and less than 128. Please try again.");
+        return;
+    } else if (isNaN(length) === true) {
+        alert("Invalid response. Please try again.");
+        return;
+    }
+    //User choices for characters. Confirm to include.
+    confirmLowerCase = confirm("Do you want to include lowercase characters in your password?");
+    confirmUpperCase = confirm("Do you want to include uppercase characters in your password?");
+    confirmNumber = confirm("Do you want to include numbers in your password?");
+    confirmSpecialCharacters = confirm("Do you want to include special characters in your password?");
+    //If user does not confirm any character types, they are alerted they must select at least one character type
+    if (confirmLowerCase === false && confirmUpperCase === false && confirmNumber === false && confirmSpecial === false) {
+        alert("Please select at least one character type.");
+        return;
+    } else { }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
