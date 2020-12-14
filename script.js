309269bdd37e710b31764b9e53292b34483e905c
Carly Peyton <carlypeyton@gmail.com>
2020-12-13 22:19:13 -0500

    Define variables and set possible string values for character types.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Set possible values for each character type. Use strings to utilize charAt
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "!#$%&*+-<>=?@^";
+
+//Define variables for each character type
+var length;
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumber;
+var confirmSpecialCharacters;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
